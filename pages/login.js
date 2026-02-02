8cc88e59b4e5f62c4863378db1f327c811831ab5
kishcok <kishock88@gmail.com>
2026-02-02 14:38:24 -0800

    Get baseURL

--- pages/login.js
+++ pages/login.js
@@ -1,25 +1,4 @@
-// exports.LoginPage = class LoginPage {
 
-//     constructor(page) {
-
-//         this.page = page
-//         this.username_textbox = page.getByLabel('Username')
-//         this.password_textbox = page.getByLabel('Password')
-//         this.login_button = page.getByRole('button', { name: 'Login' })
-//     }
-
-//     async gotoLoginPage(){
-//         await this.page.goto('https://the-internet.herokuapp.com/login');
-//     }
-
-//     async login(username, password){
-//         await this.username_textbox.fill(username)
-//         await this.password_textbox.fill(password)
-//         await this.login_button.click()
-//     }
-// }
-
- 
 exports.LoginPage = class LoginPage {
 
     constructor(page) {
@@ -33,7 +12,7 @@ exports.LoginPage = class LoginPage {
     }
 
     async gotoLoginPage() {
-        // URL은 playwright.config.js의 baseURL에서 가져오는 것
+        // Get baseURL from playwright.config.js
         //await this.page.goto('https://the-internet.herokuapp.com/login');
         await this.page.goto('/login');
     }
